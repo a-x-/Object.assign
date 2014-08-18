b48b9ca7ec5b7a4dd838243b17e5cd9c8727f1eb
Александр <a-x-@users.noreply.github.com>
2014-08-18 07:26:05 +0400

    [Nope] Change indentation to 4 spaces

--- object-assign.js
+++ object-assign.js
@@ -1,39 +1,39 @@
 (function(){
-	'use strict';
-	
-	function ToObject(val) {
-		if (val == null) {
-			throw new TypeError('Object.assign cannot be called with null or undefined');
-		}
-	
-		return Object(val);
-	}
-	
-	Object.assign || (Object.assign = function (target, source) {
-		var pendingException;
-		var from;
-		var keys;
-		var to = ToObject(target);
-	
-		for (var s = 1; s < arguments.length; s++) {
-			from = arguments[s];
-			keys = Object.keys(Object(from));
-	
-			for (var i = 0; i < keys.length; i++) {
-				try {
-					to[keys[i]] = from[keys[i]];
-				} catch (err) {
-					if (pendingException === undefined) {
-						pendingException = err;
-					}
-				}
-			}
-		}
-	
-		if (pendingException) {
-			throw pendingException;
-		}
-	
-		return to;
-	});
+    'use strict';
+    
+    function ToObject(val) {
+        if (val == null) {
+            throw new TypeError('Object.assign cannot be called with null or undefined');
+        }
+    
+        return Object(val);
+    }
+    
+    Object.assign || (Object.assign = function (target, source) {
+        var pendingException;
+        var from;
+        var keys;
+        var to = ToObject(target);
+    
+        for (var s = 1; s < arguments.length; s++) {
+            from = arguments[s];
+            keys = Object.keys(Object(from));
+    
+            for (var i = 0; i < keys.length; i++) {
+                try {
+                    to[keys[i]] = from[keys[i]];
+                } catch (err) {
+                    if (pendingException === undefined) {
+                        pendingException = err;
+                    }
+                }
+            }
+        }
+    
+        if (pendingException) {
+            throw pendingException;
+        }
+    
+        return to;
+    });
 }());
